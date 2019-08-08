fb4a184725adfa9218d2e6b54419bb62d042d79d
Theo Fournier <theo.fournier@axa.co.jp>
2019-08-08 11:25:41 +0900

    Init react-intl

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
+import { IntlProvider } from 'react-intl';
+
 import Alert from './components/common/Alert';
 
 import store from './store';
+import IntlGlobalProvider from './translations/IntlGlobalProvider';
+import englishTranslation from './translations/en.json';
+import frenchTranslation from './translations/fr.json';
+
+const translations = {
+  en: englishTranslation,
+};
+const userLanguage = (navigator.language || navigator.userLanguage).includes(
+  'fr',
+)
+  ? 'fr'
+  : 'en';
 
 const App = () => {
+  const [language, setLanguage] = useState(userLanguage);
+  const [translation, setTranslation] = useState(translations[userLanguage]);
+
+  const changeLanguage = () => {
+    if (language === 'en') {
+      setLanguage('fr');
+      setTranslation(frenchTranslation);
+    } else if (language === 'fr') {
+      setLanguage('en');
+      setTranslation(englishTranslation);
+    }
+  };
+
   return (
     <Provider store={store}>
-      <Alert />
-      <div>
-        CRM KOptical
-      </div>
+      <IntlProvider
+        locale={this.state.language}
+        messages={this.state.translations}
+      >
+        <IntlGlobalProvider>
+          <Alert />
+          <div>
+            CRM KOptical
+          </div>
+        </IntlGlobalProvider>
+
+      </IntlProvider>
     </Provider>
   );
 };
