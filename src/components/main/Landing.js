db6a0f0394081ca61ee39fbc1d1f91a3fecfa2ee
Theo Fournier <theo.fournier@axa.co.jp>
2019-08-19 11:35:16 +0900

    Landing page

--- src/components/main/Landing.js
+++ src/components/main/Landing.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { makeStyles, Typography } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
+import MyDefaultButton from '../common/MyDefaultButton';
+import logo from '../../images/logo_name.png';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexGrow: 1,
+    flexDirection: 'column',
+    margin: '100px auto 0',
+    textAlign: 'center',
+  },
+  titleContainer: {
+    margin: '0 0 80px 0',
+  },
+  logoContainer: {
+    margin: '0 0 20px 0',
+  },
 }));
 
 const Landing = () => {
   const classes = useStyles();
   return (
-    <div>
-      Landing
+    <div className={classes.root}>
+      <div className={classes.titleContainer}>
+        <img src={logo} alt="Logo" />
+        <Typography variant='h1' color="primary">
+          <FormattedMessage id='landing.title' />
+        </Typography>
+      </div>
+      <div className={classes.buttonContainer}>
+        <MyDefaultButton to='/login' component={Link} variant='default2'>
+          <FormattedMessage id='landing.login' />
+        </MyDefaultButton>
+      </div>
     </div>
   );
 };
